323d4a91a799ff0d120b3c3a1a125cf85850f8b7
rohitdeepu17 <rohitdeepu17@gmail.com>
2017-04-24 22:33:08 +0530

    Error Handling on customer details page
    Handled error : updated error to fetch bill no for all bills for a
    customer.

--- ProjectCode/customer_details.php
+++ ProjectCode/customer_details.php
@@ -99,7 +99,7 @@ function toggleShowHide(){
 			echo ("<tr><td>".$data['bill_no']."</td>");
 				$balanceamount = $data['amount']-$data['discount']-$data['paid_amount'];
 				echo("<td>".$data['bill_date']."</td><td>".round($data['amount'],2)."</td><td>".$data['discount']."</td><td>".$data['paid_amount']."</td><td>".round($balanceamount,2));
-			echo("</td><td><a href='delete_bill.php?billno=".$data['bill_no']."'><img src='delete1.png' alt='' style='width:40px; height:40px;'></a></td><td><a href='download_bill.php?billno=".$data[bill_no]."' target='_blank'><img src='download_image.png' alt='' style='width:40px; height:40px;'></a></td></tr>");
+			echo("</td><td><a href='delete_bill.php?billno=".$data['bill_no']."'><img src='delete1.png' alt='' style='width:40px; height:40px;'></a></td><td><a href='download_bill.php?billno=".$data['bill_no']."' target='_blank'><img src='download_image.png' alt='' style='width:40px; height:40px;'></a></td></tr>");
 		}
 
 	?>
@@ -128,7 +128,7 @@ function toggleShowHide(){
 			echo ("<tr><td>".$data['bill_no']."</td>");
 				$balanceamount = $data['amount']-$data['discount']-$data['paid_amount'];
 				echo("<td>".$data['bill_date']."</td><td>".round($data['amount'],2)."</td><td>".$data['discount']."</td><td>".$data['paid_amount']."</td><td>".round($balanceamount,2));
-			echo("</td><td><a href='delete_bill.php?billno=".$data['bill_no']."'><img src='delete1.png' alt='' style='width:40px; height:40px;'></a></td><td><a href='download_bill.php?billno=".$data[bill_no]."' target='_blank'><img src='download_image.png' alt='' style='width:40px; height:40px;'></a></td></tr>");
+			echo("</td><td><a href='delete_bill.php?billno=".$data['bill_no']."'><img src='delete1.png' alt='' style='width:40px; height:40px;'></a></td><td><a href='download_bill.php?billno=".$data['bill_no']."' target='_blank'><img src='download_image.png' alt='' style='width:40px; height:40px;'></a></td></tr>");
 		}
 
 	?>
